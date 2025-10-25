145254f84ca5e29e60af2d15043c0910aa21d3e6
Cmatrix1 <sadeghmajidi980@gmail.com>
2025-10-25 03:44:06 +0330

    Initial commit: Add Django project configuration and core files

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(('apps.search.urls', 'search'), namespace='search')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
