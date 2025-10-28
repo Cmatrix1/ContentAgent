7dc752584163f17bf5a5b93e569f6b19ef8a3e76
Cmatrix1 <sadeghmajidi980@gmail.com>
2025-10-29 00:09:18 +0330

    fix: order of apis in swagger

--- config/settings.py
+++ config/settings.py
@@ -186,6 +186,7 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
     'COMPONENT_SPLIT_REQUEST': True,
     'SCHEMA_PATH_PREFIX': '/api/',
+    'SORT_OPERATIONS': False,
     'SECURITY': [
         {
             'tokenAuth': [],
